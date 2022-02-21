d23f60d47b1ed643db96e1d21494bd41bba955d8
Elijah Heath <ejheath8888@gmail.com>
2022-02-21 10:09:53 -0500

    more classes with types of cars and car features

---
+++ CarLotSimulatorApp/CarLotSimulator/CarLot.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+namespace CarLotSimulator
+{
+    public class CarLot
+    {
+        public CarLot()
+        {
+        }
+
+        public List<Car> Cars = new List<Car>();
+    }
+}
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -7,18 +7,49 @@ namespace CarLotSimulator
         static void Main(string[] args)
         {
             //TODO
+            
+            //lot is an instance of the carlot class
 
-            //Create a seperate class file called Car
-            //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
-            //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
-            //The methods should take one string parameter: the respective noise property
+            var lot = new CarLot();
+           
+
+
 
 
             //Now that the Car class is created we can instanciate 3 new cars
-            //Set the properties for each of the cars
-            //Call each of the methods for each car
+            //dot notation
+            var elijahsCar = new Car();
+            elijahsCar.Make = "Ford";
+            elijahsCar.Model = "Focus";
+            elijahsCar.Year = 2012;
+            elijahsCar.EngineNoise = "vroom vroom";
+            elijahsCar.HonkNoise = "boooop";
+            elijahsCar.IsDrivable = true;
 
-            //*************BONUS*************//
+            lot.Cars.Add(elijahsCar);
+
+            //object initializer syntax
+            var michaelsCar = new Car()
+            {
+                Year = 2019,
+                Make = "Tesla",
+                Model = "Cyber Truck",
+                EngineNoise = "...",
+                HonkNoise = "errrrrr",
+                IsDrivable = false    
+            };
+
+            lot.Cars.Add(michaelsCar);
+
+            //calling it the one-liner where you need
+            //to create a new construcor to place in parameter values inside properties
+            var johnsCar = new Car(1972, "Gran Torino", "Ford", "rarrr", "eeeeee", true);
+
+            lot.Cars.Add(johnsCar);
+
+            elijahsCar.MakeEngineNoise(elijahsCar.EngineNoise);
+            michaelsCar.MakeEngineNoise(michaelsCar.EngineNoise);
+            johnsCar.MakeEngineNoise(johnsCar.EngineNoise);
 
             // Set the properties utilizing the 3 different ways we learned about, one way for each car
 
@@ -28,6 +59,11 @@ namespace CarLotSimulator
             //It should have at least one property: a List of cars
             //Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list.
             //At the end iterate through the list printing each of car's Year, Make, and Model to the console
+
+            foreach(var car in lot.Cars)
+            {
+                Console.WriteLine($"Year: {car.Year}, Make: {car.Make}, Model: {car.Model}");
+            }
         }
     }
 }
